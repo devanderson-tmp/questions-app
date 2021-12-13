250999a98aad40f62478e5b440a450fef2ebb2c2
Anderson Pires <devanderson.pires@gmail.com>
2021-12-13 15:16:43 -0300

    :heavy_plus_sign: adicionar dependência react router
    configurando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QuestionsContextProvider } from './contexts/QuestionsContext';
 import Home from './pages/Home';
 
 function App() {
 	return (
 		<QuestionsContextProvider>
-			<Home />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</BrowserRouter>
 		</QuestionsContextProvider>
 	);
 }
